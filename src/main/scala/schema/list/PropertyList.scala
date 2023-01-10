0b28e0405ae220a3c45974b578c619c3f3dbe99f
avkr003 <abhinav@assetmantle.one>
2023-01-10 16:24:56 +0530

    Revert: Adding apply for PropertyList

--- src/main/scala/schema/list/PropertyList.scala
+++ src/main/scala/schema/list/PropertyList.scala
@@ -33,6 +33,4 @@ object PropertyList {
 
   def apply(protoBytes: Array[Byte]): PropertyList = PropertyList(protoPropertyList.parseFrom(protoBytes))
 
-  def apply(anyPropertyList: Seq[AnyProperty]): PropertyList = PropertyList(anyPropertyList.map(x => Property(x)))
-
 }
