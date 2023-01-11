3073a56acb33489bebc2b35546d652d5b734f94e
mukundrawat <mukund@assetmantle.one>
2023-01-11 16:52:33 +0530

    updates

--- src/main/scala/schema/data/base/ListData.scala
+++ src/main/scala/schema/data/base/ListData.scala
@@ -2,27 +2,27 @@ package schema.data.base
 import com.data.AnyData
 import schema.data.Data
 import schema.id.base.{DataID, HashID, StringID}
-import schema.list.AnyDataList
 import com.data.{ListData => protoListData}
 
 import java.math.BigInteger
-import scala.collection.mutable.ArrayBuffer
+import scala.jdk.CollectionConverters._
 
-case class ListData(value: AnyDataList) extends Data {
+
+case class ListData(dataList: Seq[AnyData]) extends Data {
   def getType: StringID = commonConstants.DataTypeID.ListDataTypeID
 
    def getID: DataID = DataID(typeID = commonConstants.DataTypeID.ListDataTypeID, hashID = this.generateHashID)
 
-   def zeroValue: Data = ListData(value = AnyDataList(dataList = Seq()))
+   def zeroValue: Data = ListData(dataList = Seq())
 
    def generateHashID: HashID = commonUtilities.ID.generateHashID(this.getBytes)
 
-   def asProtoListData: protoListData = protoListData.newBuilder().setValue(this.value.asProtoAnyDataList).build()
+   def asProtoListData: protoListData = protoListData.newBuilder().addAllDataList(this.dataList.asJava).build()
 
    def toAnyData: AnyData = AnyData.newBuilder().setListData(this.asProtoListData).build()
 
    def getBytes: Array[Byte] = {
-      this.value.dataList.map(x => Data(x).getBytes).filter(_.length != 0).sortWith((x, y) => new BigInteger(x).compareTo(new BigInteger(y)) == -1).toArray.flatten
+      this.dataList.map(x => Data(x).getBytes).filter(_.length != 0).sortWith((x, y) => new BigInteger(x).compareTo(new BigInteger(y)) == -1).toArray.flatten
    }
 
    def getProtoBytes: Array[Byte] = this.asProtoListData.toByteArray
@@ -30,7 +30,7 @@ case class ListData(value: AnyDataList) extends Data {
 
 object ListData {
 
- def apply(value: protoListData): ListData = ListData(AnyDataList(value.getValue))
+ def apply(value: protoListData): ListData = ListData(value.getDataListList.asScala.toSeq)
 
  def apply(protoBytes: Array[Byte]): ListData = ListData(protoListData.parseFrom(protoBytes))
 
