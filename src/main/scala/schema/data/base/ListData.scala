251ab9f37b62b994d2f62ab9bd626ce523341e23
mukundrawat <mukund@assetmantle.one>
2023-01-11 17:35:10 +0530

    updates

--- src/main/scala/schema/data/base/ListData.scala
+++ src/main/scala/schema/data/base/ListData.scala
@@ -27,7 +27,7 @@ case class ListData(dataList: Seq[AnyData]) extends Data {
 
    def getProtoBytes: Array[Byte] = this.asProtoListData.toByteArray
 
-   def viewString: String = this.dataList.map(x => Data(x).viewString).filter(_.length != 0).mkString
+   def viewString: String = this.dataList.map(x => Data(x).viewString).filter(_.length != 0).mkString(", ")
 }
 
 object ListData {
